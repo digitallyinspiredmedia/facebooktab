85be97ca6484a505bcf677408e69924e1a8522d9
Kavinraj <sikavinraj@users.noreply.github.com>
2016-09-23 13:42:27 +0530

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+<title>Google Map</title>
+</head>
+<body>
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3886.424348825183!2d80.24428741437649!3d13.072270990789876!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1474616903527" width="520" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+</boby>
+</html>
